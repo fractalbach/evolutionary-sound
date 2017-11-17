a0fe6c659d2707bb019e58a07feed0b413772dcd
fractalbach <4bach@protonmail.com>
2017-11-17 09:55:05 -0800

    added waveforms
    + random number functions
    + basic waveform for oscillator
    
    > separated some functions apart

--- docs/soundCheck.md
+++ docs/soundCheck.md
@@ -22,6 +22,30 @@ const LIST_OF_WAVEFORM_TYPES = [
 ];
 ~~~
 
+Global Functions
+--------------------------------------------------------------------------
+
+During the creation of random sounds, randomly generating numbers is a common occurence.  This function generates a random integer between two integers.
+
+
+More precisely, let $f(m,M)$ be a function $f:(\mathrm{Z}, \mathrm{Z}) \to \mathrm{Z})$ such that $f(m,M) = x$ and $m \leq x \leq M$
+
+~~~ javascript 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max) + 1;
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+~~~
+
+Since the most common target is a random value from one of the global Constant Arrays, a useful function will return one of those randomly selected values.  This can be achieved by picking a random KEY in the array, with the minimum being 0, and the maximum being the length of the array.
+
+~~~ javascript 
+function getRandomValueFromArray(name) {
+    return name[ getRandomInt(0, name.length - 1) ];
+}
+~~~
+
 Audio Context
 --------------------------------------------------------------------------
 
@@ -32,32 +56,52 @@ Quote from [Mozilla Developer - Audio Context](https://developer.mozilla.org/en-
 
 
 ~~~ javascript 
-var audioContext = new AudioContext();
-var osc;
-var waveformType = "square";
-var is_sound_on = false;
+var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+~~~
+
+Simple Oscillator
+--------------------------------------------------------------------------
+
+For this example, an oscillator node will be created.  Given that we have a list of waveforms, we will choose one of them at random upon creation of the oscillator.
+
+~~~ javascript 
+var buildRandomOscillator = function () {
+    var osc;
+    var waveformType = getRandomValueFromArray(LIST_OF_WAVEFORM_TYPES);
+    osc = audioContext.createOscillator();
+    osc.type = waveformType;
+    return osc;
+};  
 ~~~
 
 Button Events - Start and Stop the Sound
 --------------------------------------------------------------------------
+
+For now, we want an oscillator to be defined globally, so that we can turn it on or off.  Before any buttons are pressed, there is no sound.  We just have an empty variable where the oscillator will go when it is turned on.
+
+~~~ javascript 
+var osc;
+var is_sound_on = false;
+~~~
 
 These functions simply start and stop the sound, and are intended to be used by button events on the HTML page.  This is part of the interface, and should always be present in some form, just in case somebody wants to shut off the sound.
 
 ~~~ javascript 
 var startSound = function() {
     if (is_sound_on) {return;}
-    osc = audioContext.createOscillator();
-    osc.type = waveformType;
+    osc = buildRandomOscillator();
     osc.connect(audioContext.destination);
     osc.start(audioContext.currentTime);
     is_sound_on = true;
     changeSoundStatus('True');
+    changeTextWaveform('Highlight', osc.type);
 };
 var endSound = function() {
     if (osc == undefined) {return;}
     osc.stop(audioContext.currentTime);
     is_sound_on = false;
     changeSoundStatus('False');
+    changeTextWaveform('NoHighlight', 'None');
 };
 ~~~
 
@@ -69,6 +113,10 @@ var changeSoundStatus = function (status) {
     var span = document.getElementById('sound_status');
     span.innerHTML = status;
     span.className = status;
+};
+var changeTextWaveform = function (status, waveform) {
+    document.getElementById('waveform_type').innerHTML = waveform;
+    document.getElementById('waveform_type').className = status;
 };
 var humanHearingCheck = function(frequency) {
     return;
--- js/soundCheck.js
+++ js/soundCheck.js
@@ -27,6 +27,25 @@ const LIST_OF_WAVEFORM_TYPES = [
 ];
 
 
+// Global Functions
+// --------------------------------------------------------------------------
+
+// During the creation of random sounds, randomly generating numbers is a common occurence.  This function generates a random integer between two integers.
+
+
+// More precisely, let $f(m,M)$ be a function $f:(\mathrm{Z}, \mathrm{Z}) \to \mathrm{Z})$ such that $f(m,M) = x$ and $m \leq x \leq M$
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max) + 1;
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Since the most common target is a random value from one of the global Constant Arrays, a useful function will return one of those randomly selected values.  This can be achieved by picking a random KEY in the array, with the minimum being 0, and the maximum being the length of the array.
+
+function getRandomValueFromArray(name) {
+    return name[ getRandomInt(0, name.length - 1) ];
+}
 
 
 // Audio Context
@@ -38,27 +57,44 @@ const LIST_OF_WAVEFORM_TYPES = [
 // > The AudioContext interface represents an audio-processing graph built from audio modules linked together, each represented by an AudioNode. An audio context controls both the creation of the nodes it contains and the execution of the audio processing, or decoding. You need to create an AudioContext before you do anything else, as everything happens inside a context.
 
 
-var audioContext = new AudioContext();
-var osc;
-var waveformType = "square";
-var is_sound_on = false;
+var audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
+
+
+// Simple Oscillator
+// --------------------------------------------------------------------------
+
+// For this example, an oscillator node will be created.  Given that we have a list of waveforms, we will choose one of them at random upon creation of the oscillator.
+
+var buildRandomOscillator = function () {
+    var osc;
+    var waveformType = getRandomValueFromArray(LIST_OF_WAVEFORM_TYPES);
+    osc = audioContext.createOscillator();
+    osc.type = waveformType;
+    return osc;
+};  
 
 
 
 // Button Events - Start and Stop the Sound 
 // --------------------------------------------------------------------------
 
+// For now, we want an oscillator to be defined globally, so that we can turn it on or off.  Before any buttons are pressed, there is no sound.  We just have an empty variable where the oscillator will go when it is turned on.
+
+var osc;
+var is_sound_on = false;
+
+
 // These functions simply start and stop the sound, and are intended to be used by button events on the HTML page.  This is part of the interface, and should always be present in some form, just in case somebody wants to shut off the sound.
 
 var startSound = function() {
     if (is_sound_on) {return;}
-    osc = audioContext.createOscillator();
-    osc.type = waveformType;
+    osc = buildRandomOscillator();
     osc.connect(audioContext.destination);
     osc.start(audioContext.currentTime);
     is_sound_on = true;
     changeSoundStatus('True');
+    changeTextWaveform('Highlight', osc.type);
 };
 
 var endSound = function() {
@@ -66,6 +102,7 @@ var endSound = function() {
     osc.stop(audioContext.currentTime);
     is_sound_on = false;
     changeSoundStatus('False');
+    changeTextWaveform('NoHighlight', 'None');
 };
 
 
@@ -79,6 +116,10 @@ var changeSoundStatus = function (status) {
 };
 
 
+var changeTextWaveform = function (status, waveform) {
+    document.getElementById('waveform_type').innerHTML = waveform;
+    document.getElementById('waveform_type').className = status;
+};
 
 
 /* Various Functions for doing Checks */
